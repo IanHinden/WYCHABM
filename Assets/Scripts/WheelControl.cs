53d9c0b4ac830b4d1940f0f3312738632d501410
Ian <Ian.Hinden@gmail.com>
2021-01-25 23:47:51 -0800

    Added ability to rotate object

---
+++ Assets/Scripts/WheelControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelControl : MonoBehaviour
+{
+    private Vector3 change;
+    public float speed;
+    private Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change.x = Input.GetAxisRaw("Horizontal");
+        Debug.Log(change.x);
+        if (change.x == -1)
+        {
+            myRigidBody.rotation += speed * Time.deltaTime;
+            Debug.Log(myRigidBody.rotation);
+        } else if (change.x == 1)
+        {
+            myRigidBody.rotation -= speed * Time.deltaTime;
+        }
+    }
+}
