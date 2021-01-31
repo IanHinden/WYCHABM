14641266b2dae79cf8344fcf13e01686ce32d750
Ian <Ian.Hinden@gmail.com>
2021-01-31 14:49:36 -0800

    Added basic example of timer object

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,6 +7,7 @@ public class CarController : MonoBehaviour
     private Vector3 change;
     public float speed;
     private Rigidbody2D myRigidBody;
+    public float timeSinceStart;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,14 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeSinceStart += Time.deltaTime;
+
         change.x = Input.GetAxisRaw("Horizontal");
-        myRigidBody.MovePosition(
-            transform.position + change * speed * Time.deltaTime
-        );
+        if (timeSinceStart > 5)
+        {
+            myRigidBody.MovePosition(
+                transform.position + change * speed * Time.deltaTime
+            );
+        }
     }
 }
