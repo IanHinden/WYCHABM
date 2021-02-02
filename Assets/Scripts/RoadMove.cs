24a5bb5cedc1fba349b733b41792869c94c16009
Ian <Ian.Hinden@gmail.com>
2021-02-01 23:10:26 -0800

    Added moving road

---
+++ Assets/Scripts/RoadMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMove : MonoBehaviour
+{
+    private Vector3 change;
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change.y = -5;
+        myRigidBody.MovePosition(
+                transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
