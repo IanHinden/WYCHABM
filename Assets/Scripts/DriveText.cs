dd4aec3537e655bf1c304cda7f30dad8ec9bf4ec
Ian <Ian.Hinden@gmail.com>
2021-02-04 22:04:03 -0800

    Added animated text instructing player to drive

---
+++ Assets/Scripts/DriveText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveText : MonoBehaviour
+{
+    public float timeSinceStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+        if (timeSinceStart > 5)
+        {
+            ToggleVisibility();
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        SpriteRenderer rend = gameObject.GetComponent<SpriteRenderer>();
+
+        rend.enabled = false;
+    }
+}
