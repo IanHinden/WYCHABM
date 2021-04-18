c6cfc11903ccdbb79604526017fd2ca09c9ca90b
Ian <Ian.Hinden@gmail.com>
2021-04-18 00:03:44 -0700

    Added intro scene, logo, and animations

---
+++ Assets/Scripts/Intro Scene/TitleFade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleFade : MonoBehaviour
+{
+
+    void Update()
+    {
+        Debug.Log("It works");      
+    }
+}
