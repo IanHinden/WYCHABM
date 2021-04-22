a074ecce8c269d5e1d7b2901ec7d0b175b7c854f
Ian <Ian.Hinden@gmail.com>
2021-04-21 22:09:45 -0700

    Added character icon for Start and Credits on mouse over

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.transform.GetChild(1).gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
