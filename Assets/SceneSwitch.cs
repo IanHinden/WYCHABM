ab2317f0887b243f137bc931e7a06a052c1f2549
Ian <Ian.Hinden@gmail.com>
2021-04-24 22:08:23 -0700

    Added music player and ability to switch to different tracks

--- Assets/SceneSwitch.cs
+++ Assets/SceneSwitch.cs
@@ -5,8 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+    public AudioClip newTrack;
+
+    private MusicPlayer theMP;
+
+    private void Start()
+    {
+        theMP = FindObjectOfType<MusicPlayer>();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        theMP.ChangeBGM(newTrack);
     }
 }
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void ChangeBGM(AudioClip music)
+    {
+        audioSource.Stop();
+        audioSource.clip = music;
+        audioSource.Play();
+    }
+}
