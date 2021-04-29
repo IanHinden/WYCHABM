057aa03321c45ad64d412c2975e956f21dbc1e0a
Ian <Ian.Hinden@gmail.com>
2021-04-28 23:17:14 -0700

    Added script to default characters to deselected color

---
+++ Assets/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer girl = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        girl.color = new Color32(126, 126, 126, 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
