f594d31cfb81b68c5cfa45d6cdd732b16c744af9
Ian <Ian.Hinden@gmail.com>
2021-05-02 21:32:33 -0700

    Added scrolling city image

---
+++ Assets/Scripts/City and Bar Intro/CityBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CityBehavior : MonoBehaviour
+{
+    GameObject cityScape;
+
+    float scrollSpeed = 35f;
+    private void Awake()
+    {
+        cityScape = GameObject.Find("Canvas").transform.Find("CityScape").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cityScape.transform.position += new Vector3(1, 1, 0) * Time.deltaTime * scrollSpeed;
+    }
+}
