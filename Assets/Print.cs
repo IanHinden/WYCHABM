1d819ae7da8cab5f0f5188d8036521672882bc7a
Ian <Ian.Hinden@gmail.com>
2021-05-03 23:20:17 -0700

    Pen writes (badly) when moved

---
+++ Assets/Print.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Print : MonoBehaviour
+{
+    [SerializeField] public GameObject ink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void spawnInk()
+    {
+        GameObject inkBlot = Instantiate(ink) as GameObject;
+        inkBlot.transform.position = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        spawnInk();
+    }
+}
