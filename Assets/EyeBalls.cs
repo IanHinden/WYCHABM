01a707dbdaa114d05dffe639feabef015bfb5563
Ian <Ian.Hinden@gmail.com>
2021-05-04 22:40:35 -0700

    Eyeballs point down when signing contract now

---
+++ Assets/EyeBalls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBalls : MonoBehaviour
+{
+    private WritingControls writingControls;
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        writingControls = new WritingControls();
+    }
+
+    private void OnEnable()
+    {
+        writingControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        writingControls.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementInput = writingControls.Move.Directions.ReadValue<Vector2>();
+        Vector3 currentPosition = transform.position;
+        if (movementInput != Vector2.zero)
+        {
+            currentPosition.y += -1f * speed * Time.deltaTime;
+            currentPosition.y = Mathf.Clamp(currentPosition.y, -.04f, 0f);
+            transform.position = currentPosition;
+        } else
+        {
+            transform.position = new Vector2(0, 0);
+        }
+
+    }
+}
