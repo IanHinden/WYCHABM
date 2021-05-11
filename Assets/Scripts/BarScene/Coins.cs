0a3593c5fada5ceba9532803bd011f5762ae6eab
Ian <Ian.Hinden@gmail.com>
2021-05-10 19:09:15 -0700

    Added SuccessOrFail object and SuccessOrFail trigger

---
+++ Assets/Scripts/BarScene/Coins.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    SuccessOrFail successOrFail;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        successOrFail = new SuccessOrFail();
+        StartCoroutine(WinOrLose());
+    }
+
+    public int GetRemainingCoins()
+    {
+        int remainingCoins = gameObject.transform.childCount;
+        return remainingCoins;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int remainingCoins = GetRemainingCoins();
+        if(remainingCoins == 0)
+        {
+            successOrFail.WinDisplay();
+        }
+    }
+
+    IEnumerator WinOrLose()
+    {
+        yield return new WaitForSeconds(1);
+        DetermineWinOrLoss();
+    }
+
+    private void DetermineWinOrLoss()
+    {
+        int remainingCoins = GetRemainingCoins();
+        if(remainingCoins == 0)
+        {
+            successOrFail.WinDisplay();
+        } else
+        {
+            successOrFail.LoseDisplay();
+        }
+    }
+}
---
+++ Assets/Scripts/SuccessOrFail.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuccessOrFail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void WinDisplay()
+    {
+        Debug.Log("Win!");
+    }
+
+    public void LoseDisplay()
+    {
+        Debug.Log("Lose");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
