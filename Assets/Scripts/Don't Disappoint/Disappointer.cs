936e7b0c5728636bfaf1c0d6134b1a682300848c
Ian <Ian.Hinden@gmail.com>
2021-05-22 22:28:03 -0700

    Added logic to Don't Disappoint scene

---
+++ Assets/Scripts/Don't Disappoint/Disappointer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappointer : MonoBehaviour
+{
+    SuccessOrFail successOrFail;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        successOrFail = gameObject.AddComponent<SuccessOrFail>();
+        StartCoroutine(WinOrLose());
+    }
+
+    IEnumerator WinOrLose()
+    {
+        yield return new WaitForSeconds(4f);
+        DetermineWinOrLoss();
+    }
+
+    private void DetermineWinOrLoss()
+    {
+        Debug.Log("It happen");
+        successOrFail.LoseDisplay();
+    }
+}
