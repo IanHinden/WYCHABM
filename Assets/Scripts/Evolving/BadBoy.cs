3b423d61472a214ae6d31547f2e676bec9f94c39
Ian <Ian.Hinden@gmail.com>
2021-05-23 18:17:41 -0700

    Added blinking Bad Boy animation and scrolling text

--- Assets/Scripts/Evolving/BadBoy.cs
+++ Assets/Scripts/Evolving/BadBoy.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BadBoy : MonoBehaviour
 {
+    private Image badBoy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        badBoy = gameObject.GetComponent<Image>();
+        StartCoroutine(Blink());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Blink()
     {
-        
+        yield return new WaitForSeconds(1);
+        badBoy.enabled = false;
+        yield return new WaitForSeconds(1);
+        badBoy.enabled = true;
+        yield return new WaitForSeconds(1);
+        badBoy.enabled = false;
     }
 }
---
+++ Assets/Scripts/Evolving/EvolveText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EvolveText : MonoBehaviour
+{
+    private TextMeshProUGUI textmesh;
+    private float waitTime = 2.6f;
+    private string dialogue = "BAD BOY evolved into BAD MAN!";
+
+    void Start()
+    {
+        textmesh = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(TypeLyrics());
+    }
+
+    IEnumerator TypeLyrics()
+    {
+        while (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        textmesh.text = "";
+
+        foreach (char c in dialogue.ToCharArray())
+        {
+            textmesh.text += c;
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
