71ac28f38b401ec6e28ef5ebca02892e2e3f0baa
Ian <Ian.Hinden@gmail.com>
2021-05-29 23:04:18 -0700

    Fixed timing of loss on disappointed scene (for anyone reading this, you can not win this scene)

--- Assets/Scripts/Don't Disappoint/Disappointer.cs
+++ Assets/Scripts/Don't Disappoint/Disappointer.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Disappointer : MonoBehaviour
 {
     SuccessOrFail successOrFail;
+    ThreeSecondsLeft threeSecondsLeft;
+    SceneSwitch sceneSwitch;
     // Start is called before the first frame update
     void Awake()
     {
         successOrFail = gameObject.AddComponent<SuccessOrFail>();
+        threeSecondsLeft = gameObject.AddComponent<ThreeSecondsLeft>();
+        sceneSwitch = FindObjectOfType<SceneSwitch>();
         StartCoroutine(WinOrLose());
     }
 
     IEnumerator WinOrLose()
     {
-        yield return new WaitForSeconds(4f);
+        float deadline = sceneSwitch.ReturnTimeToSwitch() - threeSecondsLeft.ReturnTimeToEnd() + (3 * threeSecondsLeft.ReturnSingleMeasure());
+        while (deadline > 0)
+        {
+            deadline -= Time.deltaTime;
+            yield return null;
+        }
         DetermineWinOrLoss();
     }
 
