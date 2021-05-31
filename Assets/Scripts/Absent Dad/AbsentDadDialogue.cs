ab8b6f51846618a838d2df9b9d79e9e2c5b716a9
Ian <Ian.Hinden@gmail.com>
2021-05-30 22:17:06 -0700

    Added graphics and scrolling text for absent dad scene

---
+++ Assets/Scripts/Absent Dad/AbsentDadDialogue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AbsentDadDialogue : MonoBehaviour
+{
+    private TextMeshProUGUI textmesh;
+    private float waitTime = 1f;
+    private string dialogue = "Sorry, kiddo. Tell mom I won't be home. I got a lot of work to finish at the office. ";
+
+    void Start()
+    {
+        textmesh = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(TypeLyrics());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator TypeLyrics()
+    {
+        while (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        textmesh.text = "MR. RICHMOND:";
+
+        foreach (char c in dialogue.ToCharArray())
+        {
+            textmesh.text += c;
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
