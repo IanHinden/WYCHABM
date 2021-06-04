32c29f0aeeebeb7b52ba189af8eaac096719190c
Ian <Ian.Hinden@gmail.com>
2021-06-03 23:15:59 -0700

    Added initial graphics to web surfing and wishlist scenes

---
+++ Assets/Scripts/Wishlist/SelectChecker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectChecker : MonoBehaviour
+{
+    SelectArrow[] selectArrows;
+    int activeArrow = 2;
+    void Start()
+    {
+        selectArrows = FindObjectsOfType<SelectArrow>();
+    }
+
+    public void displayCorrectArrow()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if (i != activeArrow) {
+                Debug.Log(selectArrows[i]);
+                selectArrows[i].enabled = false;
+            }
+        }
+    }
+
+    public void setNextActiveArrow()
+    {
+        if(activeArrow != 3)
+        {
+            activeArrow++;
+        } else
+        {
+            activeArrow = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayCorrectArrow();
+    }
+}
