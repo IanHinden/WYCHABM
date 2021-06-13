100fd247606cfc4e7bb559794b8fb6f0d91af2e8
Ian <Ian.Hinden@gmail.com>
2021-06-13 12:28:53 -0700

    Scaled graphics for all screen sizes

--- Assets/Scripts/Title Scene/PlayButton.cs
+++ Assets/Scripts/Title Scene/PlayButton.cs
@@ -5,16 +5,5 @@ using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
-    public GameObject button;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //button.transform.GetChild(1).gameObject.SetActive(false);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
