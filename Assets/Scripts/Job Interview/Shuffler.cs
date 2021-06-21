9793fecf4bb1534b015862bd45948987938d234d
Ian <Ian.Hinden@gmail.com>
2021-06-20 22:50:32 -0700

    Added Job Interview scene and initial dialogue shuffle animation

---
+++ Assets/Scripts/Job Interview/Shuffler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuffler : MonoBehaviour
+{
+    private SpeechBubble[] speechBubbles;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        speechBubbles = FindObjectsOfType<SpeechBubble>();
+        speechBubbles[1].transform.SetSiblingIndex(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
