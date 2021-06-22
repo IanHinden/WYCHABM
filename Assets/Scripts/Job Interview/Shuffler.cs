f0fc452fa43dadb3989ebe4b65eed60e047a2c05
Ian <Ian.Hinden@gmail.com>
2021-06-21 22:41:43 -0700

    Added functions for shuffling dialogue options to the right and left

--- Assets/Scripts/Job Interview/Shuffler.cs
+++ Assets/Scripts/Job Interview/Shuffler.cs
@@ -17,8 +17,38 @@ public class Shuffler : MonoBehaviour
     IEnumerator Test()
     {
         yield return new WaitForSeconds(4);
-        Debug.Log("Yeah!");
+        speechBubbles[0].GetComponent<Animator>().SetTrigger("BottomToMid");
+        speechBubbles[1].GetComponent<Animator>().SetTrigger("MidToTop");
+        speechBubbles[2].GetComponent<Animator>().SetTrigger("TopToBottom");
+        speechBubbles[1].transform.SetSiblingIndex(2);
+        speechBubbles[0].transform.SetSiblingIndex(1);
+
+        yield return new WaitForSeconds(4);
+        Debug.Log("2nd time");
+        speechBubbles = FindObjectsOfType<SpeechBubble>().OrderBy(m => m.transform.position.x).ToArray();
+        speechBubbles[0].GetComponent<Animator>().SetTrigger("BottomToMid");
+        speechBubbles[1].GetComponent<Animator>().SetTrigger("MidToTop");
+        speechBubbles[2].GetComponent<Animator>().SetTrigger("TopToBottom");
+        speechBubbles[1].transform.SetSiblingIndex(2);
+        speechBubbles[0].transform.SetSiblingIndex(1);
+    }
+
+    private void ShuffleLeft()
+    {
+        speechBubbles = FindObjectsOfType<SpeechBubble>().OrderBy(m => m.transform.position.x).ToArray();
+        speechBubbles[0].GetComponent<Animator>().SetTrigger("BottomToTop");
         speechBubbles[1].GetComponent<Animator>().SetTrigger("MidToBottom");
+        speechBubbles[2].GetComponent<Animator>().SetTrigger("TopToMid");
+        speechBubbles[0].transform.SetSiblingIndex(2);
+    }
+    private void ShuffleRight()
+    {
+        speechBubbles = FindObjectsOfType<SpeechBubble>().OrderBy(m => m.transform.position.x).ToArray();
+        speechBubbles[0].GetComponent<Animator>().SetTrigger("BottomToMid");
+        speechBubbles[1].GetComponent<Animator>().SetTrigger("MidToTop");
+        speechBubbles[2].GetComponent<Animator>().SetTrigger("TopToBottom");
+        speechBubbles[1].transform.SetSiblingIndex(2);
+        speechBubbles[0].transform.SetSiblingIndex(1);
     }
 
     // Update is called once per frame
