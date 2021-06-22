952e0c71a6bf417429313239e1c3b8151ccf9047
Ian <Ian.Hinden@gmail.com>
2021-06-21 22:16:02 -0700

    Added animation logic to Object 2

--- Assets/Scripts/Job Interview/Shuffler.cs
+++ Assets/Scripts/Job Interview/Shuffler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Shuffler : MonoBehaviour
@@ -8,8 +9,16 @@ public class Shuffler : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        speechBubbles = FindObjectsOfType<SpeechBubble>();
-        speechBubbles[1].transform.SetSiblingIndex(0);
+        speechBubbles = FindObjectsOfType<SpeechBubble>().OrderBy(m => m.transform.position.x).ToArray();
+        //speechBubbles[1].transform.SetSiblingIndex(0);
+        StartCoroutine(Test());
+    }
+
+    IEnumerator Test()
+    {
+        yield return new WaitForSeconds(4);
+        Debug.Log("Yeah!");
+        speechBubbles[1].GetComponent<Animator>().SetTrigger("MidToBottom");
     }
 
     // Update is called once per frame
