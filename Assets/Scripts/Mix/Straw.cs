6c9af0e116df0a30fa4f55153458b2e5dbc70289
Ian <Ian.Hinden@gmail.com>
2021-06-24 22:49:22 -0700

    Added controls for straw

---
+++ Assets/Scripts/Mix/Straw.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Straw : MonoBehaviour
+{
+    private Stir stir;
+    private float speed = 1.5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        stir = new Stir();
+    }
+
+    private void OnEnable()
+    {
+        stir.Enable();
+    }
+
+    private void OnDisable()
+    {
+        stir.Disable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float selectInput = stir.MoveStraw.Move.ReadValue<float>();
+        float currentPosition = transform.position.x;
+
+        currentPosition += selectInput * speed * Time.deltaTime;
+        currentPosition = Mathf.Clamp(currentPosition, -.87f, .22f);
+
+        transform.position = new Vector3(currentPosition, transform.position.y, transform.position.z);
+    }
+}
