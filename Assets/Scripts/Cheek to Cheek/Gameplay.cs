9ea757187f741a4244f4b7a5c87e577a00f39df0
Ian <Ian.Hinden@gmail.com>
2021-07-10 00:29:24 -0700

    Added initial scripts, sprites, and text

---
+++ Assets/Scripts/Cheek to Cheek/Gameplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    private RichmondLips richmondLips;
+    // Start is called before the first frame update
+    void Start()
+    {
+        richmondLips = FindObjectOfType<RichmondLips>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(richmondLips.transform.position.y);
+    }
+}
