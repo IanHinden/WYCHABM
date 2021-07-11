349c4c79d165cb84c9164a4d82e681a207767048
Ian <Ian.Hinden@gmail.com>
2021-07-10 21:55:18 -0700

    Added script to move spotlight in circle

---
+++ Assets/Scripts/Cheek to Cheek/Spotlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour
+{
+    private float RotateSpeed = 5f;
+    private float Radius = 2f;
+
+    private Vector2 _centre;
+    private float _angle;
+    void Start()
+    {
+        _centre = new Vector3(4.6f, 2.2f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _angle += RotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+        transform.position = _centre + offset;
+    }
+}
