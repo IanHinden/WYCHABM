1d1d203e8872f722e2024740083317022a826d05
Ian <Ian.Hinden@gmail.com>
2021-07-10 22:34:34 -0700

    Added controls

--- Assets/Scripts/Cheek to Cheek/Gameplay.cs
+++ Assets/Scripts/Cheek to Cheek/Gameplay.cs
@@ -6,17 +6,37 @@ public class Gameplay : MonoBehaviour
 {
     private RichmondLips richmondLips;
     private Spotlight spotlight;
+    private KissHit kissHit;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         richmondLips = FindObjectOfType<RichmondLips>();
         spotlight = FindObjectOfType<Spotlight>();
+
+        kissHit = new KissHit();
+        kissHit.Action.Select.performed += x => GameAction();
+    }
+
+
+    private void OnEnable()
+    {
+        kissHit.Enable();
+    }
+
+    private void OnDisable()
+    {
+        kissHit.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(richmondLips.transform.position.y);
+        //Debug.Log(richmondLips.transform.position.y);
         spotlight.RotateSpotlight();
+    }
+
+    private void GameAction()
+    {
+        Debug.Log("Game action");
     }
 }
