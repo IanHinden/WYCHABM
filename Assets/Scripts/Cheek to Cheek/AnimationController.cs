53aba39fbfaf9c512972a29e642e853b60fc3998
Ian <Ian.Hinden@gmail.com>
2021-07-14 23:14:21 -0700

    Added lean in kiss for winning kiss animation

--- Assets/Scripts/Cheek to Cheek/AnimationController.cs
+++ Assets/Scripts/Cheek to Cheek/AnimationController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     Animator heartAnim;
+    Rigidbody2D richmondLipsRB;
 
     void Awake()
     {
         heartAnim = FindObjectOfType<Heart>().GetComponent<Animator>();
+        richmondLipsRB = FindObjectOfType<RichmondLips>().GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -19,5 +21,14 @@ public class AnimationController : MonoBehaviour
     public void KissWinAnimation()
     {
         heartAnim.SetTrigger("Success");
+        StartCoroutine(KissMove());
+    }
+
+    private IEnumerator KissMove()
+    {
+        richmondLipsRB.velocity = transform.right * -2;
+        yield return new WaitForSeconds(.5f);
+        richmondLipsRB.velocity = Vector3.zero;
+        yield return null;
     }
 }
