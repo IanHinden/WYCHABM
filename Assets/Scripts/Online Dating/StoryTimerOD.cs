664640e0164c396fd19c044bb5372ec2c4a227f3
Ian <Ian.Hinden@gmail.com>
2021-07-28 23:47:10 -0700

    Added online dating scene and sprites

---
+++ Assets/Scripts/Online Dating/StoryTimerOD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTimerOD : MonoBehaviour
+{
+    private ThreeSecondsLeft threeSecondsLeft;
+
+    private SpriteRenderer flashing;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        threeSecondsLeft = gameObject.AddComponent<ThreeSecondsLeft>();
+
+        flashing = FindObjectOfType<Topless>().GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
