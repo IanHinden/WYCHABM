c215b597c6e0212060c34966cb9568a0df07a24d
Ian <Ian.Hinden@gmail.com>
2021-07-29 00:05:57 -0700

    Added tweak scene

--- Assets/Scripts/Online Dating/StoryTimerOD.cs
+++ Assets/Scripts/Online Dating/StoryTimerOD.cs
@@ -6,18 +6,40 @@ public class StoryTimerOD : MonoBehaviour
 {
     private ThreeSecondsLeft threeSecondsLeft;
 
+    private float measureMS;
+    private float timePassed = 0f;
+
+    private Topless topless;
     private SpriteRenderer flashing;
+    private Animator flashingMove;
     // Start is called before the first frame update
     void Awake()
     {
         threeSecondsLeft = gameObject.AddComponent<ThreeSecondsLeft>();
+        measureMS = threeSecondsLeft.ReturnSingleMeasure();
 
-        flashing = FindObjectOfType<Topless>().GetComponent<Animator>();
+        topless = FindObjectOfType<Topless>();
+        flashing = topless.GetComponent<SpriteRenderer>();
+        flashingMove = topless.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timePassed += Time.deltaTime;
+        timedEvents();
+    }
+
+    private void timedEvents()
+    {
+        if (timePassed > measureMS * 2)
+        {
+            flashing.enabled = true;
+        }
+
+        if(timePassed > measureMS * 3)
+        {
+            flashingMove.SetTrigger("Flash");
+        }
     }
 }
