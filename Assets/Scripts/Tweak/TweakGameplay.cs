4fc77ca854c0d11ec0ea22687bf3967dd981b96d
Ian <Ian.Hinden@gmail.com>
2021-07-30 23:28:49 -0700

    Added controls to rotate directional highlight

---
+++ Assets/Scripts/Tweak/TweakGameplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class TweakGameplay : MonoBehaviour
+{
+    ControlPadButton[] controlPadButtons;
+
+    private float currentlySelected = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controlPadButtons = FindObjectsOfType<ControlPadButton>().OrderBy(go => go.name).ToArray();
+        Debug.Log(controlPadButtons[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RotateRight()
+    {
+        if(currentlySelected == 3)
+        {
+            currentlySelected = 0;
+            displayCorrectArrow();
+        } else
+        {
+            currentlySelected++;
+            displayCorrectArrow();
+        }
+    }
+
+    private void displayCorrectArrow()
+    {
+        for (int i = 0; i < controlPadButtons.Length; i++)
+        {
+            if (i != currentlySelected)
+            {
+                controlPadButtons[i].GetComponent<SpriteRenderer>().enabled = false;
+            }
+            else
+            {
+                controlPadButtons[i].GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+    }
+}
