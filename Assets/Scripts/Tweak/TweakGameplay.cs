48f5a4e472aad4bdfcd5953c50abef73f9ccb333
Ian <Ian.Hinden@gmail.com>
2021-07-31 21:27:18 -0700

    Added direction controls

--- Assets/Scripts/Tweak/TweakGameplay.cs
+++ Assets/Scripts/Tweak/TweakGameplay.cs
@@ -6,18 +6,96 @@ public class TweakGameplay : MonoBehaviour
 {
     ControlPadButton[] controlPadButtons;
 
+    private TweakControls tweakControls;
+
     private float currentlySelected = 0;
+    private float state = 0;
     // Start is called before the first frame update
     void Awake()
     {
         controlPadButtons = FindObjectsOfType<ControlPadButton>().OrderBy(go => go.name).ToArray();
-        Debug.Log(controlPadButtons[0]);
+        
+        tweakControls = new TweakControls();
+        tweakControls.Move.UpArrow.performed += x => upMove();
+        tweakControls.Move.RightArrow.performed += x => rightMove();
+        tweakControls.Move.DownArrow.performed += x => downMove();
+        tweakControls.Move.LeftArrow.performed += x => leftMove();
+    }
+
+    private void upMove()
+    {
+        if (state == 0)
+        {
+            state++;
+            RotateRight();
+        }
+
+        if(state == 4)
+        {
+            state++;
+            RotateRight();
+        }
+    }
+
+    private void rightMove()
+    {
+        if (state == 1)
+        {
+            state++;
+            RotateRight();
+        }
+
+        if (state == 5)
+        {
+            state++;
+            RotateRight();
+        }
+    }
+
+    private void downMove()
+    {
+        if (state == 2)
+        {
+            state++;
+            RotateRight();
+        }
+
+        if (state == 6)
+        {
+            state++;
+            RotateRight();
+        }
+    }
+
+    private void leftMove()
+    {
+        if (state == 3)
+        {
+            state++;
+            RotateRight();
+        }
+
+        if (state == 7)
+        {
+            state++;
+            RotateRight();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        tweakControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        tweakControls.Disable();
     }
 
     private void RotateRight()
