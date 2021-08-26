39a8887238bee189cd68065e066c86e573f43a27
Ian <Ian.Hinden@gmail.com>
2021-08-25 21:36:34 -0700

    Added graphics and set up animation timing script

---
+++ Assets/Scripts/Bar View/BarViewTiming.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarViewTiming : MonoBehaviour
+{
+    private ThreeSecondsLeft threeSecondsLeft;
+
+    private float measureMS;
+    private float timePassed = 0f;
+    void Awake()
+    {
+        threeSecondsLeft = gameObject.AddComponent<ThreeSecondsLeft>();
+        measureMS = threeSecondsLeft.ReturnSingleMeasure();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        timedEvents();
+    }
+
+    private void timedEvents()
+    {
+        if (timePassed > measureMS * 4)
+        {
+            Debug.Log("A measure");
+        }
+    }
+}
