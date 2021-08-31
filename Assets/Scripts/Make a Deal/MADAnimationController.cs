a950a3f48035269392e92e8ab8355cd05dff5291
Ian <Ian.Hinden@gmail.com>
2021-08-30 22:18:56 -0700

    Added assets and animated Make a Deal scene

---
+++ Assets/Scripts/Make a Deal/MADAnimationController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MADAnimationController : MonoBehaviour
+{
+    Animator evilSmirkAnim;
+    Animator dialogueBoxAnim;
+
+    private TextMeshPro textmesh;
+    private float waitTime = 2f;
+    public string dialogue;
+    void Awake()
+    {
+        evilSmirkAnim = FindObjectOfType<EvilSmirk>().GetComponent<Animator>();
+        dialogueBoxAnim = FindObjectOfType<DialogueBox>().GetComponent<Animator>();
+        textmesh = FindObjectOfType<BobDylan>().GetComponent<TextMeshPro>();
+        StartCoroutine(TypeLyrics());
+
+        evilSmirkAnim.SetTrigger("Enter");
+        dialogueBoxAnim.SetTrigger("FadeIn");
+    }
+
+    IEnumerator TypeLyrics()
+    {
+        while (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        textmesh.text = "";
+
+        foreach (char c in dialogue.ToCharArray())
+        {
+            textmesh.text += c;
+            float pauseTime = .02f;
+
+            while (pauseTime > 0)
+            {
+                pauseTime -= Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
