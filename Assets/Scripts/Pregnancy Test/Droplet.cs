fe99b6e724f9a978d2a36d966ac407fe468155a3
Ian <Ian.Hinden@gmail.com>
2021-09-03 23:14:43 -0700

    Added logic for droplet

---
+++ Assets/Scripts/Pregnancy Test/Droplet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droplet : MonoBehaviour
+{
+    public float speed = 50.0f;
+    private Rigidbody2D rb;
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
