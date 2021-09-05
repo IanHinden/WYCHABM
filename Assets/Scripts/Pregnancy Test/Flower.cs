4bc0e5bc47b380d69724bf8d299eef324b6280a7
Ian <Ian.Hinden@gmail.com>
2021-09-04 21:45:06 -0700

    Added prefabs and initial spawning script

---
+++ Assets/Scripts/Pregnancy Test/Flower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    public GameObject smallDroplet;
+    public GameObject mediumDroplet;
+    public GameObject largeDroplet;
+
+    public float respawnTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(spray());
+    }
+
+    private void spawnDroplet()
+    {
+        GameObject droplet = Instantiate(smallDroplet) as GameObject;
+        droplet.transform.position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    IEnumerator spray()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnDroplet();
+        }
+    }
+}
