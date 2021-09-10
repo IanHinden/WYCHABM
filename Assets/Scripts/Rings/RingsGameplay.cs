c896a529f33837b06235271c55414112a15cc10c
Ian <Ian.Hinden@gmail.com>
2021-09-09 23:38:41 -0700

    Added initial controls

---
+++ Assets/Scripts/Rings/RingsGameplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingsGameplay : MonoBehaviour
+{
+    Remove remove;
+
+    void Awake()
+    {
+        remove = new Remove();
+        remove.Tap.Up.performed += x => RemoveRing(); 
+    }
+
+    private void OnEnable()
+    {
+        remove.Enable();
+    }
+
+    private void OnDisable()
+    {
+        remove.Disable();
+    }
+
+    private void RemoveRing()
+    {
+        Debug.Log("Hey");
+    }
+}
