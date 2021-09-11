cadd4e2e24fd3260ac57db9d36fee9bf8f238895
Ian <Ian.Hinden@gmail.com>
2021-09-11 00:00:25 -0700

    Added button instructions animation

---
+++ Assets/Scripts/Rings/RingsAnimationController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingsAnimationController : MonoBehaviour
+{
+    [SerializeField] GameObject SpaceUp;
+    [SerializeField] GameObject SpaceDown;
+
+    private float timePassed = 0f;
+
+    void Awake()
+    {
+        SpaceUp.SetActive(true);
+        SpaceDown.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timePassed += Time.deltaTime;
+        Debug.Log(timePassed);
+
+        if(timePassed > 1f && timePassed < 2f)
+        {
+            SpaceUp.SetActive(false);
+            SpaceDown.SetActive(true);
+        }
+
+        if (timePassed > 2f && timePassed < 3f)
+        {
+            SpaceUp.SetActive(true);
+            SpaceDown.SetActive(false);
+        }
+
+        if (timePassed > 3f && timePassed < 4f)
+        {
+            SpaceUp.SetActive(false);
+            SpaceDown.SetActive(true);
+        }
+
+        if (timePassed > 4f && timePassed < 5f)
+        {
+            SpaceUp.SetActive(true);
+            SpaceDown.SetActive(false);
+        }
+
+        if (timePassed > 5f && timePassed < 6f)
+        {
+            SpaceUp.SetActive(false);
+            SpaceDown.SetActive(true);
+        }
+    }
+}
