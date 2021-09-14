4e85766fc55aac75fce89357549ccbb18f53fc36
Ian <Ian.Hinden@gmail.com>
2021-09-13 22:48:42 -0700

    Moved Big Pitties Text out and shortned dialogue

--- Assets/Scripts/City and Bar Intro/CityBehavior.cs
+++ Assets/Scripts/City and Bar Intro/CityBehavior.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CityBehavior : MonoBehaviour
 {
     [SerializeField] GameObject city;
 
     Animator avaAnimator;
+    Animator ppgAnimator;
 
     PittiePartyDialogue pittiePartyDialogue;
+
+    TextMeshPro bigPittiesText;
+
     void Awake()
     {
         StartCoroutine(StartAnimations());
 
         avaAnimator = FindObjectOfType<Ava>().GetComponent<Animator>();
+        ppgAnimator = FindObjectOfType<PittiePartyGirl>().GetComponent<Animator>();
+
         pittiePartyDialogue = FindObjectOfType<PittiePartyDialogue>();
+        bigPittiesText = FindObjectOfType<BigPittiesText>().GetComponent<TextMeshPro>();
     }
 
     void Update()
@@ -24,10 +32,23 @@ public class CityBehavior : MonoBehaviour
 
     IEnumerator StartAnimations()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2f);
         city.SetActive(false);
+        bigPittiesText.text = "";
 
         avaAnimator.SetTrigger("Enter");
+        yield return new WaitForSeconds(1.5f);
         StartCoroutine(pittiePartyDialogue.SetAvaDialogue());
+        yield return new WaitForSeconds(1.5f);
+        ppgAnimator.SetTrigger("Enter");
+        yield return new WaitForSeconds(2f);
+        avaAnimator.SetTrigger("FadeOut");
+        StartCoroutine(pittiePartyDialogue.SetPpgDialogue());
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(pittiePartyDialogue.SetPpgDialogueTwo());
+        yield return new WaitForSeconds(1.5f);
+        ppgAnimator.SetTrigger("Exit");
+        avaAnimator.SetTrigger("FadeIn");
+        StartCoroutine(pittiePartyDialogue.SetAvaDialogueTwo());
     }
 }
