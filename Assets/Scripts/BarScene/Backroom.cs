8dae741ded2599e56d02bcb7fedd95ac6b76a47d
Ian <Ian.Hinden@gmail.com>
2021-09-20 23:00:33 -0700

    Added backroom and stolen wages

---
+++ Assets/Scripts/BarScene/Backroom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backroom : MonoBehaviour
+{
+    CameraMovement cameraMovement;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cameraMovement = FindObjectOfType<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(cameraMovement.MoveToBackroom());
+    }
+}
---
+++ Assets/Scripts/BarScene/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public IEnumerator MoveToBackroom()
+    {
+        Vector3 targetPosition = new Vector3(-17.8f, 0f, -10f);
+
+        float elapsedTime = 0;
+        float waitTime = 20f;
+
+        while (elapsedTime < waitTime)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, (elapsedTime/waitTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
