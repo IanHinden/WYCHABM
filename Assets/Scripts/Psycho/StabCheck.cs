96ab1fc46a7a7367b34657fe4bff00833f1ce2d0
Ian <Ian.Hinden@gmail.com>
2021-10-05 08:58:42 -0700

    Work done to move arrested evelopment assets off of canvas

--- Assets/Scripts/Psycho/StabCheck.cs
+++ Assets/Scripts/Psycho/StabCheck.cs
@@ -7,6 +7,9 @@ public class StabCheck : MonoBehaviour
     //SuccessOrFail successOrFail;
     ThreeSecondsLeft threeSecondsLeft;
     SceneSwitch sceneSwitch;
+
+    Animator starAnim;
+
     public GameObject oedipalBonus;
 
     bool stabbed = false;
@@ -14,19 +17,24 @@ public class StabCheck : MonoBehaviour
 
     private void Awake()
     {
-        //successOrFail = gameObject.AddComponent<SuccessOrFail>();
         threeSecondsLeft = FindObjectOfType<ThreeSecondsLeft>();
         sceneSwitch = FindObjectOfType<SceneSwitch>();
+
+        starAnim = threeSecondsLeft.transform.Find("CountdownImages").transform.GetChild(3).transform.GetChild(4).GetComponent<Animator>();
         StartCoroutine(WinOrLose());
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.name == "OedipalBonus")
+        if (collision.name == "OedipalBonus")
         {
             Vector3 textPos = transform.position;
             textPos.x = transform.position.x - 3;
             Instantiate(oedipalBonus, textPos, Quaternion.identity);
             oedipal = true;
+            if(stabbed == false)
+            {
+                threeSecondsLeft.DisplayBonusScoreCard(starAnim);
+            }
         }
         stabbed = true;
         threeSecondsLeft.WinDisplay();
