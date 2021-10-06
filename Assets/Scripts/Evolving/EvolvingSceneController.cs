2ed50c34ee38cf4ee3128a5ab0a80c291b08cff7
Ian <Ian.Hinden@gmail.com>
2021-10-05 21:31:43 -0700

    Added Pokemon font, text, and logic to cancel evolution

---
+++ Assets/Scripts/Evolving/EvolvingSceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolvingSceneController : MonoBehaviour
+{
+
+    EvolvingText evolvingText;
+    StopEvolving stopEvolving;
+
+    private IEnumerator evolving;
+    private IEnumerator stopped;
+    
+    void Awake()
+    {
+        evolvingText = FindObjectOfType<EvolvingText>();
+        stopEvolving = new StopEvolving();
+        stopEvolving.Stop.StopEvolve.performed += x => StopEvolution();
+        evolving = evolvingText.SetEvolvingDialogue();
+        stopped = evolvingText.SetStoppedDialogue();
+        StartCoroutine(evolving);
+    }
+
+    private void StopEvolution()
+    {
+        StopCoroutine(evolving);
+        StartCoroutine(stopped);
+    }
+
+    private void OnEnable()
+    {
+        stopEvolving.Enable();
+    }
+
+    private void OnDisable()
+    {
+        stopEvolving.Disable();
+    }
+}
