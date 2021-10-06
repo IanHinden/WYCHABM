e54cae4940559a03386aaef79cf6c03569a7d445
Ian <Ian.Hinden@gmail.com>
2021-10-05 22:43:59 -0700

    Added logic for blinking and the stopping of blinking

--- Assets/Scripts/Evolving/EvolvingSceneController.cs
+++ Assets/Scripts/Evolving/EvolvingSceneController.cs
@@ -8,23 +8,46 @@ public class EvolvingSceneController : MonoBehaviour
     EvolvingText evolvingText;
     StopEvolving stopEvolving;
 
+    public GameObject BadBoy;
+    SpriteRenderer badBoySR;
+
     private IEnumerator evolving;
     private IEnumerator stopped;
+    private IEnumerator blinking;
     
     void Awake()
     {
         evolvingText = FindObjectOfType<EvolvingText>();
         stopEvolving = new StopEvolving();
+        badBoySR = BadBoy.GetComponent<SpriteRenderer>();
+
         stopEvolving.Stop.StopEvolve.performed += x => StopEvolution();
         evolving = evolvingText.SetEvolvingDialogue();
         stopped = evolvingText.SetStoppedDialogue();
+        blinking = Blinking();
+
         StartCoroutine(evolving);
+        StartCoroutine(blinking);
     }
 
     private void StopEvolution()
     {
         StopCoroutine(evolving);
+        StopCoroutine(blinking);
+        badBoySR.enabled = true;
         StartCoroutine(stopped);
+    }
+
+    private IEnumerator Blinking()
+    {
+        badBoySR.enabled = true;
+        yield return new WaitForSeconds(.4f);
+        badBoySR.enabled = false;
+        yield return new WaitForSeconds(.4f);
+        badBoySR.enabled = true;
+        yield return new WaitForSeconds(.4f);
+        badBoySR.enabled = false;
+        yield return new WaitForSeconds(.4f);
     }
 
     private void OnEnable()
