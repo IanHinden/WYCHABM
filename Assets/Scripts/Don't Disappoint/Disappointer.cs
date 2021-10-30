6bcec8d3c575fd3283ccad467863d5605567f8ea
Ian <Ian.Hinden@gmail.com>
2021-10-29 22:26:41 -0700

    Removed canvas elements and reference to Scene Switcher for victory animations

--- Assets/Scripts/Don't Disappoint/Disappointer.cs
+++ Assets/Scripts/Don't Disappoint/Disappointer.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 
 public class Disappointer : MonoBehaviour
 {
-    SuccessOrFail successOrFail;
     ThreeSecondsLeft threeSecondsLeft;
-    SceneSwitch sceneSwitch;
 
     void Awake()
     {
-        successOrFail = gameObject.AddComponent<SuccessOrFail>();
         threeSecondsLeft = FindObjectOfType<ThreeSecondsLeft>();
-        sceneSwitch = FindObjectOfType<SceneSwitch>();
         StartCoroutine(WinOrLose());
     }
 
     IEnumerator WinOrLose()
     {
-        float deadline = (2 * threeSecondsLeft.ReturnTimeToEnd()) - threeSecondsLeft.ReturnSingleMeasure();
+        float deadline = threeSecondsLeft.ReturnTimeToEnd() - threeSecondsLeft.ReturnSingleMeasure();
         yield return new WaitForSeconds(deadline);
         DetermineWinOrLoss();
     }
 
     private void DetermineWinOrLoss()
     {
-        successOrFail.LoseDisplay();
+        threeSecondsLeft.LoseDisplay();
     }
 }
