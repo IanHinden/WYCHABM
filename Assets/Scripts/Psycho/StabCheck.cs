cac303592d2d226a389c58166c2829a06a525304
Ian <Ian.Hinden@gmail.com>
2021-11-02 21:18:07 -0700

    Fixed logic for win and lose animations

--- Assets/Scripts/Psycho/StabCheck.cs
+++ Assets/Scripts/Psycho/StabCheck.cs
@@ -6,7 +6,6 @@ public class StabCheck : MonoBehaviour
 {
     //SuccessOrFail successOrFail;
     ThreeSecondsLeft threeSecondsLeft;
-    SceneSwitch sceneSwitch;
 
     Animator starAnim;
 
@@ -14,11 +13,11 @@ public class StabCheck : MonoBehaviour
 
     bool stabbed = false;
     bool oedipal = false;
+    bool levelEnded = false;
 
     private void Awake()
     {
         threeSecondsLeft = FindObjectOfType<ThreeSecondsLeft>();
-        sceneSwitch = FindObjectOfType<SceneSwitch>();
 
         starAnim = threeSecondsLeft.transform.Find("CountdownImages").transform.GetChild(3).transform.GetChild(4).GetComponent<Animator>();
         StartCoroutine(WinOrLose());
@@ -27,17 +26,23 @@ public class StabCheck : MonoBehaviour
     {
         if (collision.name == "OedipalBonus")
         {
-            Vector3 textPos = transform.position;
-            textPos.x = transform.position.x - 3;
-            Instantiate(oedipalBonus, textPos, Quaternion.identity);
-            if(oedipal == false)
+            if (levelEnded == false)
             {
-                threeSecondsLeft.DisplayBonusScoreCard(starAnim);
+                Vector3 textPos = transform.position;
+                textPos.x = transform.position.x - 3;
+                Instantiate(oedipalBonus, textPos, Quaternion.identity);
+                if (oedipal == false)
+                {
+                    threeSecondsLeft.DisplayBonusScoreCard(starAnim);
+                    oedipal = true;
+                }
             }
         }
-        oedipal = true;
         stabbed = true;
-        threeSecondsLeft.WinDisplay();
+        if (levelEnded == false)
+        {
+            threeSecondsLeft.WinDisplay();
+        }
     }
 
     IEnumerator WinOrLose()
@@ -50,8 +55,9 @@ public class StabCheck : MonoBehaviour
 
     private void DetermineWinOrLoss()
     {
-        if (stabbed == false)
+        if (stabbed == false && levelEnded == false)
         {
+            levelEnded = true;
             threeSecondsLeft.LoseDisplay();
         }
     }
