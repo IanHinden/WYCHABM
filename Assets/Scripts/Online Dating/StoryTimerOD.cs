fc358598ef9965b26bb4235c47e7ce11a499f1ea
Ian <Ian.Hinden@gmail.com>
2021-11-03 22:19:44 -0700

    Moved update timing into Coroutine

--- Assets/Scripts/Online Dating/StoryTimerOD.cs
+++ Assets/Scripts/Online Dating/StoryTimerOD.cs
@@ -7,39 +7,26 @@ public class StoryTimerOD : MonoBehaviour
     private ThreeSecondsLeft threeSecondsLeft;
 
     private float measureMS;
-    private float timePassed = 0f;
 
-    private Topless topless;
+    [SerializeField] private Topless topless;
     private SpriteRenderer flashing;
     private Animator flashingMove;
-    // Start is called before the first frame update
+
     void Awake()
     {
         threeSecondsLeft = FindObjectOfType<ThreeSecondsLeft>();
         measureMS = threeSecondsLeft.ReturnSingleMeasure();
 
-        topless = FindObjectOfType<Topless>();
         flashing = topless.GetComponent<SpriteRenderer>();
         flashingMove = topless.GetComponent<Animator>();
+        StartCoroutine(SceneTiming());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SceneTiming()
     {
-        timePassed += Time.deltaTime;
-        timedEvents();
-    }
-
-    private void timedEvents()
-    {
-        if (timePassed > measureMS * 2)
-        {
-            flashing.enabled = true;
-        }
-
-        if(timePassed > measureMS * 3)
-        {
-            flashingMove.SetTrigger("Flash");
-        }
+        yield return new WaitForSeconds(measureMS * 8f);
+        flashing.enabled = true;
+        yield return new WaitForSeconds(measureMS * 1);
+        flashingMove.SetTrigger("Flash");
     }
 }
