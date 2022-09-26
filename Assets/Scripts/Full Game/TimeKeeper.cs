222bf5a06b5ebfc7c494377b8455a9c8d4d4d7dc
Ian <Ian.Hinden@gmail.com>
2022-09-26 08:40:33 -0700

    Confirmed timekeeper can switch camera

--- Assets/Scripts/Full Game/TimeKeeper.cs
+++ Assets/Scripts/Full Game/TimeKeeper.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class TimeKeeper : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        StartCoroutine(SwitchScene());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(2);
+        Camera.current.transform.Translate(61.1f, 0, 0);
+
     }
 }
