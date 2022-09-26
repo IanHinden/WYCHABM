3a4a5f76442a6d7b11c25d01bcb8fb3ed982274d
Ian <Ian.Hinden@gmail.com>
2022-09-25 21:43:36 -0700

    Added CameraMover object and TimeKeeper object

---
+++ Assets/Scripts/Full Game/CameraMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    private float[][] coordinates = new float[][] {
+        new float[2] {0f, 0f},
+        new float[2] { 61.1f, 0f}
+    };
+
+    private int currentCoordinates = 0;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchCamera()
+    {
+        currentCoordinates++;
+        Camera.current.transform.Translate(coordinates[currentCoordinates][0], coordinates[currentCoordinates][1], 0);
+
+    }
+}
