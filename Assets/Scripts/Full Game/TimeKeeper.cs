1726816d31e210b67257847ce5ace3cb72288ba9
Ian <Ian.Hinden@gmail.com>
2022-10-04 21:48:29 -0700

    Added function to switch scenario and added timing for initial scenes

--- Assets/Scripts/Full Game/TimeKeeper.cs
+++ Assets/Scripts/Full Game/TimeKeeper.cs
@@ -4,31 +4,44 @@ using UnityEngine;
 
 public class TimeKeeper : MonoBehaviour
 {
+    ArrayList allscenes = new ArrayList();
+    int currentScene = 0;
+
     [SerializeField] private GameObject CityAndBarIntro;
     [SerializeField] private GameObject BarScene;
+    [SerializeField] private GameObject Fired;
     [SerializeField] private GameObject Contract;
     // Start is called before the first frame update
+    //0.705882
+
     void Awake()
     {
+        allscenes.Add(CityAndBarIntro);
+        allscenes.Add(BarScene);
+        allscenes.Add(Fired);
+        allscenes.Add(Contract);
         StartCoroutine(SwitchScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void nextScene()
     {
-        
+        GameObject nextActiveScene = (GameObject)allscenes[currentScene + 1];
+        GameObject currentActiveScene = (GameObject)allscenes[currentScene];
+
+        currentActiveScene.SetActive(false);
+        nextActiveScene.SetActive(true);
+
+        currentScene++;
     }
 
     IEnumerator SwitchScene()
     {
-        yield return new WaitForSeconds(3);
-        CityAndBarIntro.SetActive(false);
-        BarScene.SetActive(true);
-        Camera.current.transform.Translate(61.1f, 0, 0);
-        yield return new WaitForSeconds(3);
-        BarScene.SetActive(false);
-        Contract.SetActive(true);
-        Camera.current.transform.Translate(30.3f, 0, 0);
-
+        yield return new WaitForSeconds(14.1176f);
+        nextScene();
+        yield return new WaitForSeconds(5.647f);
+        nextScene();
+        yield return new WaitForSeconds(1.411f);
+        nextScene();
+        yield return new WaitForSeconds(4.235f);
     }
 }
