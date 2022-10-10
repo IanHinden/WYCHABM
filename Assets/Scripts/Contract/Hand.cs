cf00891ad422cbe8ae0cbcc35a641c98f2be3d05
Ian <Ian.Hinden@gmail.com>
2022-10-09 22:35:52 -0700

    Added initial attempt to prevent constant ink spawning

--- Assets/Scripts/Contract/Hand.cs
+++ Assets/Scripts/Contract/Hand.cs
@@ -45,7 +45,11 @@ public class Hand : MonoBehaviour
         currentPosition.x = Mathf.Clamp(currentPosition.x, 2f, 3.5f);
         currentPosition.y = Mathf.Clamp(currentPosition.y, -1.8f, -1.2f);
 
-        print.InkSpawner();
+        //Need a better solution to stop constant calls
+        if (movementInput.x < 0 || movementInput.y < 0 || movementInput.x > 0 || movementInput.y > 0)
+        {
+            print.InkSpawner();
+        }
 
         transform.position = currentPosition;
     }
