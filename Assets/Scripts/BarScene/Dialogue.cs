604c7dc6b2e04778a4a3efb1fa2a141192ce1855
Ian <Ian.Hinden@gmail.com>
2022-11-01 22:59:17 -0700

    Added a dialogue prefab with functions to enter, exit, and change text

---
+++ Assets/Scripts/BarScene/Dialogue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    private TextMeshPro textmesh;
+    private Animator anim;
+
+    void Awake()
+    {
+        textmesh = this.gameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>();
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    public void DialogueEnter()
+    {
+        anim.SetBool("Entered", true);
+    }
+
+    public void DialogueExit()
+    {
+        anim.SetBool("Entered", false);
+    }
+
+    public IEnumerator SetDialogue(string dialogue)
+    {
+        foreach (char c in dialogue.ToCharArray())
+        {
+            textmesh.text += c;
+            float pauseTime = .02f;
+
+            while (pauseTime > 0)
+            {
+                pauseTime -= Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Full Game/2. Bar Scene/BarScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarScene : MonoBehaviour
+{
+    [SerializeField] Dialogue dialogue;
+    void Awake()
+    {
+        StartCoroutine(Dialogue());
+    }
+
+    private IEnumerator Dialogue()
+    {
+        yield return new WaitForSeconds(2);
+        dialogue.DialogueEnter();
+        yield return new WaitForSeconds(1);
+        StartCoroutine(dialogue.SetDialogue("Rich man Richmond get the itch, man"));
+        yield return new WaitForSeconds(1);
+        dialogue.DialogueExit();
+    }
+}
