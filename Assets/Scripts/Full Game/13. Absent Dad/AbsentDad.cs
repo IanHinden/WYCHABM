f22e5fee4e9e706dd4ffcdd470ed1393f0fb6487
Ian <Ian.Hinden@gmail.com>
2022-11-03 23:00:34 -0700

    Switched to dialogue functions

---
+++ Assets/Scripts/Full Game/13. Absent Dad/AbsentDad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsentDad : MonoBehaviour
+{
+    [SerializeField] Dialogue dialogue;
+    void Awake()
+    {
+        dialogue.transform.position = new Vector2(0, -6.5f);
+        StartCoroutine(Dialogue());
+    }
+
+    private IEnumerator Dialogue()
+    {
+        yield return new WaitForSeconds(.3f);
+        dialogue.DialogueEnter();
+        StartCoroutine(dialogue.SetDialogue("Sorry, kiddo. Tell mom I won't be home. I got a lot of work to finish at the office. "));
+    }
+}
---
+++ Assets/Scripts/Full Game/14. Psycho/Psycho.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Psycho : MonoBehaviour
+{
+    [SerializeField] Dialogue dialogue;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        dialogue.QuickExit();
+    }
+}
