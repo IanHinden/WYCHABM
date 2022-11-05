d3ef313c1ec411dd0bd5b2840a097b5430850de9
Ian <Ian.Hinden@gmail.com>
2022-11-04 23:37:49 -0700

    Added 'Stealth' command to bedside scene

---
+++ Assets/Scripts/Full Game/34. Bedside/Bedside.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bedside : MonoBehaviour
+{
+    [SerializeField] UIHandler uihandler;
+    [SerializeField] TimeFunctions timefunctions;
+    void Awake()
+    {
+        StartCoroutine(StartAnimations());
+    }
+
+    // Update is called once per frame
+    IEnumerator StartAnimations()
+    {
+        yield return new WaitForSeconds(3 * timefunctions.ReturnSingleMeasure());
+        uihandler.InstructionText("STEALTH");
+    }
+}
