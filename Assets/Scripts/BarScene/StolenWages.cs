aaa886c85726f2b415994e4542e2a702a8fef86a
Ian <Ian.Hinden@gmail.com>
2022-12-25 23:39:55 -0800

    Changed out test background and added score logic

--- Assets/Scripts/BarScene/StolenWages.cs
+++ Assets/Scripts/BarScene/StolenWages.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StolenWages : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI displayscore;
     public static event Action<int> WagesGet = delegate { };
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        displayscore.text = "2000/6";
         WagesGet(1);
         Destroy(gameObject);
     }
