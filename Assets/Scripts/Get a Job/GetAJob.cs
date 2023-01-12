8384d766c50571f5f6f0dfc0bed82fbeaa51a5e8
Ian <Ian.Hinden@gmail.com>
2023-01-11 23:04:22 -0800

    Added camera animation for Get A Job scene

---
+++ Assets/Scripts/Get a Job/GetAJob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAJob : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+
+    Animator CamAnim;
+    void Awake()
+    {
+        CamAnim = cam.GetComponent<Animator>();
+        CamAnim.SetTrigger("GetAJob");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
