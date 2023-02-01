f57e1acac7051294354a7bc18e4ae71341eaf469
Ian <Ian.Hinden@gmail.com>
2023-01-31 20:49:27 -0800

    Removed wrong sprites from Absent Dad / Added test loading screen:

---
+++ Assets/Scripts/Instructions/AsyncController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AsyncController : MonoBehaviour
+{
+    [SerializeField] private GameObject startButton;
+
+    [SerializeField] private Slider loadingSlider;
+
+    public void LoadLevelBtn(string levelToLoad)
+    {
+        StartCoroutine(LoadLevelAsync(levelToLoad));
+    }
+
+    IEnumerator LoadLevelAsync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+        
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            yield return null;
+        }
+    }
+}
