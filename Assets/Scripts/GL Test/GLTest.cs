a33324057155cf7d4a02db836562d3cf6f264946
Ian <Ian.Hinden@gmail.com>
2023-02-22 22:50:54 -0800

    Initial shape test

---
+++ Assets/Scripts/GL Test/GLTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GLTest : MonoBehaviour
+{
+    float width = 1;
+    float height = 1;
+    private void Awake()
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[4];
+
+        vertices[0] = new Vector3(-width, -height);
+        vertices[1] = new Vector3(-width, height);
+        vertices[2] = new Vector3(width, height);
+        vertices[3] = new Vector3(width, -height);
+
+        mesh.vertices = vertices;
+
+        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+}
