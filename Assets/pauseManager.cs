d957d1a13bd1f9cc8e36f074923a171d9cc2ee10
Gavin Arechiga <gavinarechiga@gmail.com>
2023-04-16 12:11:27 -0700

    Pause Functionality & Text Organization
    Pause Button now added to screen that will stop music and game time all at once, it is designed to be very easily accessible so that if we wish to add keyboard functionality we can do so. We would have to reassess the input system though if these changes are to be made.
    
    While the buttons are hooked up, the main menu button has been deactivated pending development on scene management bug.
    
    Text organization consisted of moving known scene specific fonts into a separate folder with the idea being that it would create a repeatable structure for any additional fonts. It is fairly miniscule currently until more scene specific fonts can be attributed to it.

---
+++ Assets/pauseManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseManager : MonoBehaviour
+{
+    public AudioSource musicPlayer;
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+    private bool isPaused = false;
+
+    public void pauseUnpauseGame()
+    {
+        GameObject titleScreen = GameObject.Find("2. Title Screen");
+
+        if (titleScreen != null)
+        {
+
+            if (titleScreen.activeInHierarchy == false)
+            {
+                //when pause function is called and the menu is still up
+                if (isPaused)
+                {
+                    isPaused = false;
+                    musicPlayer.pitch = 1;
+                    Time.timeScale = 1;
+                    pauseMenu.SetActive(false);
+                    pauseButton.SetActive(true);
+                }
+                //when pause function is called and the menu is hidden
+                else if (isPaused == false)
+                {
+                    isPaused = true;
+                    musicPlayer.pitch = 0;
+                    Time.timeScale = 0;
+                    pauseMenu.SetActive(true);
+                    pauseButton.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            //when pause function is called and the menu is still up
+            if (isPaused)
+            {
+                isPaused = false;
+                musicPlayer.pitch = 1;
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+                pauseButton.SetActive(true);
+            }
+            //when pause function is called and the menu is hidden
+            else if (isPaused == false)
+            {
+                isPaused = true;
+                musicPlayer.pitch = 0;
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                pauseButton.SetActive(false);
+            }
+        }
+    }
+
+    public void quitGame()
+    {
+
+    }
+
+    public void backToMainMenu()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void instructions()
+    {
+
+    }
+}
