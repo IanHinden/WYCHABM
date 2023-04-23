914f406fd3fc680ed6b3250123e06a223c4f2d1d
Gavin Arechiga <gavinarechiga@gmail.com>
2023-04-23 13:42:50 -0700

    First Steps for Rhythm Game
    Framework for rhythm game introduced with the next steps being to better integrate it with each chorus section and to get it to fully deliver a ranking system based off of the performance.
    
    Additionally, I fixed a bug with the pen drawing line where it wasn't leaving the screen after the scene had ended

---
+++ Assets/detectionSquare.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class detectionSquare : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        print("Entered");
+        //based on distance from point, deliver rating
+    }
+}
---
+++ Assets/rhythmGameSetUp.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rhythmGameSetUp : MonoBehaviour
+{
+    private GameControls gameControls;
+    public CircleCollider2D leftArrowDetector;
+    public CircleCollider2D upArrowDetector;
+    public CircleCollider2D downArrowDetector;
+    public CircleCollider2D rightArrowDetector;
+    public List<GameObject> leftArrows;
+    public List<GameObject> upArrows;
+    public List<GameObject> downArrows;
+    public List<GameObject> rightArrows;
+    public List<GameObject> leftArrowExamples;
+    public List<GameObject> downArrowExamples;
+    public List<GameObject> upArrowExamples;
+    public List<GameObject> rightArrowExamples;
+
+    void Awake()
+    {
+        gameControls = new GameControls();
+    }
+
+    private void OnEnable()
+    {
+        gameControls.Enable();
+    }
+
+    public void OnDisable()
+    {
+        gameControls.Disable();
+    }
+
+    public void addExampleArrow(string direction)
+    {
+        if (direction == "Left" || direction == "left")
+        {
+            leftArrowExamples[0].SetActive(true);
+            leftArrowExamples.Remove(leftArrowExamples[0]);
+        }
+        if (direction == "Right" || direction == "right")
+        {
+            rightArrowExamples[0].SetActive(true);
+            rightArrowExamples.Remove(rightArrowExamples[0]);
+        }
+        if (direction == "Up" || direction == "up")
+        {
+            upArrowExamples[0].SetActive(true);
+            upArrowExamples.Remove(upArrowExamples[0]);
+        }
+        if (direction == "Down" || direction == "down")
+        {
+            downArrowExamples[0].SetActive(true);
+            downArrowExamples.Remove(downArrowExamples[0]);
+        }
+    }
+
+    public void addArrow(string direction)
+    {
+        if (direction == "Left" || direction == "left")
+        {
+            leftArrows[0].SetActive(true);
+            leftArrows.Remove(leftArrows[0]);
+        }
+        if (direction == "Right" || direction == "right")
+        {
+            rightArrows[0].SetActive(true);
+            rightArrows.Remove(rightArrows[0]);
+        }
+        if (direction == "Up" || direction == "up")
+        {
+            upArrows[0].SetActive(true);
+            upArrows.Remove(upArrows[0]);
+        }
+        if (direction == "Down" || direction == "down")
+        {
+            downArrows[0].SetActive(true);
+            downArrows.Remove(downArrows[0]);
+        }
+    }
+
+    void Update()
+    {
+        Vector2 keyInput = gameControls.Move.Directions.ReadValue<Vector2>();
+
+        if (keyInput.x < 0)
+        {
+            //left key
+            leftArrowDetector.enabled = true;
+            StartCoroutine(buttonReset(leftArrowDetector));
+        }
+        if (keyInput.x > 0)
+        {
+            //right key
+            rightArrowDetector.enabled = true;
+            StartCoroutine(buttonReset(rightArrowDetector));
+        }
+        if (keyInput.y < 0)
+        {
+            //down key
+            downArrowDetector.enabled = true;
+            StartCoroutine(buttonReset(downArrowDetector));
+        }
+        if (keyInput.y > 0)
+        {
+            //up key
+            upArrowDetector.enabled = true;
+            StartCoroutine(buttonReset(upArrowDetector));
+        }
+    }
+
+    IEnumerator buttonReset(CircleCollider2D detectorToReset)
+    {
+        yield return new WaitForSeconds(0.01f);
+        detectorToReset.enabled = false;
+    }
+}
