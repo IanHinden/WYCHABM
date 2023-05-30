23e8411ba428d217c823d73e99879e8a6b1237dc
Ian <Ian.Hinden@gmail.com>
2023-05-29 19:32:47 -0700

    Added Fired graphics and tears animation

---
+++ Assets/Scripts/Fired/FiredTears.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiredTears : MonoBehaviour
+{
+    SpriteRenderer tears;
+    void Awake()
+    {
+        tears = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tears.color = new Color(1f, 1f, 1f, Random.Range(.7f, 1f));
+    }
+}
