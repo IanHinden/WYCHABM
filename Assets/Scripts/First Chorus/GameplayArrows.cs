b5a53fbbd363b6c2cd7cacf803c3283e38bfbd4c
Ian <Ian.Hinden@gmail.com>
2023-06-17 18:55:57 -0700

    Added note pattern for first chorus

---
+++ Assets/Scripts/First Chorus/GameplayArrows.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayArrows : MonoBehaviour
+{
+    float speed = 200f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);    
+    }
+}
