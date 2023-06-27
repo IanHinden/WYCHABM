f779706b88bc64155c5865582276308a66950b71
Ian <Ian.Hinden@gmail.com>
2023-06-26 22:14:27 -0700

    Fixed bug where user couldn't click on Start Game / Added perfect and not bad displays for Chorus 1

--- Assets/detectionSquare.cs
+++ Assets/detectionSquare.cs
@@ -5,13 +5,31 @@ using UnityEngine.U2D;
 
 public class detectionSquare : MonoBehaviour
 {
+    [SerializeField] GameObject perfect;
+    [SerializeField] GameObject good;
     void OnTriggerEnter2D(Collider2D col)
     {
-        print("Entered");
-        //based on distance from point, deliver rating
         float distance = Vector3.Distance(transform.position, col.gameObject.transform.position);
         Debug.Log(distance);
-
+        if(distance <= 3)
+        {
+            SpawnPerfect();
+        } else if (distance > 3 && distance < 7)
+        {
+            SpawnGood();
+        }
         Destroy(col.gameObject);
+    }
+
+    private void SpawnPerfect()
+    {
+        GameObject perfectSign = Instantiate(perfect, new Vector3(640f, 360f, 0), Quaternion.identity);
+        perfectSign.transform.SetParent(transform);
+    }
+
+    private void SpawnGood()
+    {
+        GameObject goodSign = Instantiate(good, new Vector3(640f, 360f, 0), Quaternion.identity);
+        goodSign.transform.SetParent(transform);
     }
 }
