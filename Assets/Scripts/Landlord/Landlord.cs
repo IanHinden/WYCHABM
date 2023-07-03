08ef08e9924b6989453e3c7f00a2aa110cd06cef
Ian <Ian.Hinden@gmail.com>
2023-07-02 19:06:28 -0700

    Added lighning effect to Landlord scene

--- Assets/Scripts/Landlord/Landlord.cs
+++ Assets/Scripts/Landlord/Landlord.cs
@@ -11,6 +11,9 @@ public class Landlord : MonoBehaviour
     [SerializeField] UIHandler uihandler;
     [SerializeField] TimeFunctions timefunctions;
 
+    [SerializeField] GameObject lightning;
+    SpriteRenderer lightningSR;
+
     Animator landlordAnim;
     Animator avaAnim;
     void Awake()
@@ -18,7 +21,8 @@ public class Landlord : MonoBehaviour
         landlordAnim = landlord.GetComponent<Animator>();
         avaAnim = ava.GetComponent<Animator>();
         dialogue.transform.position = new Vector2(0, -6.5f);
-        //StartCoroutine(Dialogue());
+        lightningSR = lightning.GetComponent<SpriteRenderer>();
+        StartCoroutine(LightningFlash());
     }
 
     public IEnumerator Dialogue()
@@ -34,5 +38,23 @@ public class Landlord : MonoBehaviour
     {
         landlord.transform.position = new Vector3(-14.02f, -1.41f, 0);
         ava.transform.position = new Vector3(11.42f, -1.82f, 0);
+    }
+
+    //TODO This might need to go into a non-Awake function for reset
+    private IEnumerator LightningFlash()
+    {
+        yield return new WaitForSeconds(3);
+        lightningSR.color = new Color(1, 1, 1, 1);
+        yield return new WaitForSeconds(.03f);
+        lightningSR.color = new Color(1, 1, 1, 0);
+        yield return new WaitForSeconds(.03f);
+        lightningSR.color = new Color(1, 1, 1, 1);
+        yield return new WaitForSeconds(.03f);
+        lightningSR.color = new Color(1, 1, 1, 0);
+        yield return new WaitForSeconds(.03f);
+        lightningSR.color = new Color(1, 1, 1, 1);
+        yield return new WaitForSeconds(.03f);
+        lightningSR.color = new Color(1, 1, 1, 0);
+        yield return new WaitForSeconds(.03f);
     }
 }
