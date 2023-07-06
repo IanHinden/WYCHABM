14e1d4a19883fd22e1620ade670d232af1aef89b
Ian <Ian.Hinden@gmail.com>
2023-07-05 23:06:18 -0700

    Added dad and portable gaming device animations

---
+++ Assets/Scripts/Get a Job/GetAJobTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAJobTime : MonoBehaviour
+{
+    [SerializeField] TimeFunctions timeFunctions;
+
+    [SerializeField] GameObject eyesPlaying;
+    [SerializeField] GameObject eyesShocked;
+    [SerializeField] GameObject portableGamingDeviceInHand;
+    [SerializeField] GameObject portableGamingDeviceFalling;
+
+    private Animator portableGamingDeviceAnimation;
+
+    void Awake()
+    {
+        portableGamingDeviceAnimation = portableGamingDeviceFalling.GetComponent<Animator>(); 
+        StartCoroutine(AllEvents());
+    }
+
+    IEnumerator AllEvents()
+    {
+        yield return new WaitForSeconds(2f);
+        eyesPlaying.SetActive(false);
+        eyesShocked.SetActive(true);
+        portableGamingDeviceInHand.SetActive(false);
+        portableGamingDeviceFalling.SetActive(true);
+        portableGamingDeviceAnimation.SetTrigger("Drop");
+    }
+}
