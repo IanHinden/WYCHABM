432203af25b1a666515fae63e6160a98e469335c
Ian <Ian.Hinden@gmail.com>
2023-07-09 22:08:31 -0700

    Added new sprites to Party Girl scenes

--- Assets/Scripts/Party Girls/StoryTimerPG.cs
+++ Assets/Scripts/Party Girls/StoryTimerPG.cs
@@ -7,36 +7,18 @@ public class StoryTimerPG : MonoBehaviour
     [SerializeField] TimeFunctions timefunctions;
 
     private float measureMS;
+    public float rotationSpeed = 1f;
+    public float rotationDuration = 2f;
 
-    private Animator gothGirl;
-
-    private SpriteRenderer bubbleBackground;
-
-    private Animator clubGirl1;
-
-    // Start is called before the first frame update
     void Awake()
     {
         measureMS = timefunctions.ReturnSingleMeasure();
 
-        gothGirl = FindObjectOfType<GothGirl>().GetComponent<Animator>();
-
-        bubbleBackground = FindObjectOfType<BubbleBackground>().GetComponent<SpriteRenderer>();
-
-        clubGirl1 = FindObjectOfType<ClubGirl1>().GetComponent<Animator>();
-
-        StartCoroutine(timedEvents());
+        //StartCoroutine(timedEvents());
     }
 
     private IEnumerator timedEvents()
     {
-        yield return new WaitForSeconds(measureMS);
-        gothGirl.SetTrigger("Doorway");
-
-        yield return new WaitForSeconds(3 * measureMS);
-        bubbleBackground.enabled = true;
-
-        yield return new WaitForSeconds(measureMS);
-        clubGirl1.SetTrigger("Pose");
+        yield return new WaitForSeconds(5f);
     }
 }
