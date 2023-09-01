847063573c9c7519716d30601c616679bd3b0323
Ian <Ian.Hinden@gmail.com>
2023-08-31 23:23:34 -0700

    Fixerd proportions / shadow grows instead of shrinks

--- Assets/Scripts/Car Arrival/Avas.cs
+++ Assets/Scripts/Car Arrival/Avas.cs
@@ -6,10 +6,17 @@ public class Avas : MonoBehaviour
 {
     private GameObject Ava1;
     private GameObject Ava2;
+
+    [SerializeField] GameObject shadows;
+    private GameObject shadow1;
+    private GameObject shadow2;
     void Awake()
     {
         Ava1 = transform.GetChild(0).gameObject;
         Ava2 = transform.GetChild(1).gameObject;
+
+        shadow1 = shadows.transform.GetChild(0).gameObject;
+        shadow2 = shadows.transform.GetChild(1).gameObject;
         StartCoroutine(Sway());
     }
 
@@ -17,6 +24,7 @@ public class Avas : MonoBehaviour
     {
         yield return new WaitForSeconds(1.3f);
         Ava1.SetActive(true);
+        shadow1.SetActive(true);
         yield return new WaitForSeconds(.7f);
         Swap();
         yield return new WaitForSeconds(.3f);
@@ -39,6 +47,8 @@ public class Avas : MonoBehaviour
     private void Swap()
     {
         Ava1.SetActive(!Ava1.activeSelf);
-        Ava2.SetActive(!Ava2.activeSelf); 
+        Ava2.SetActive(!Ava2.activeSelf);
+        shadow1.SetActive(!shadow1.activeSelf);
+        shadow2.SetActive(!shadow2.activeSelf);
     }
 }
