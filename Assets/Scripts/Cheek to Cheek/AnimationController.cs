0e38e4b80d3bf04ba84670e1b94290dc00130c7d
Ian <Ian.Hinden@gmail.com>
2023-09-18 23:06:29 -0700

    Removed all old sprites and scripts from Cheek to Cheek and added new ones

--- Assets/Scripts/Cheek to Cheek/AnimationController.cs
+++ Assets/Scripts/Cheek to Cheek/AnimationController.cs
@@ -4,119 +4,11 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-    [SerializeField] Heart heart;
-    Animator heartAnim;
-
-    [SerializeField] RichmondLips richmondLips;
-    Rigidbody2D richmondLipsRB;
-
-    [SerializeField] Missus missus;
-    Rigidbody2D missusRB;
-
-    [SerializeField] LeftHeart leftHeart;
-    [SerializeField] RightHeart rightHeart;
-    Animator leftHeartAnim;
-    Animator rightHeartAnim;
-
-    [SerializeField] KissCover kiss;
-    [SerializeField] HitCover hit;
-    Animator kissCover;
-    Animator hitCover;
-
-    [SerializeField] SpankArm spankArm;
-    [SerializeField] SpankMark spankMark;
-    Animator spankMarkAnim;
-
-    [SerializeField] Legs legs;
-    [SerializeField] Spotlight spotLight;
-
     private bool kissTriggered = false;
     private bool hitTriggered = false;
 
     void Awake()
     {
-        heartAnim = heart.GetComponent<Animator>();
-        richmondLipsRB = richmondLips.GetComponent<Rigidbody2D>();
-        missusRB = missus.GetComponent<Rigidbody2D>();
-        spankMarkAnim = spankMark.GetComponent<Animator>();
-        leftHeartAnim = leftHeart.GetComponent<Animator>();
-        rightHeartAnim = rightHeart.GetComponent<Animator>();
-        kissCover = kiss.GetComponent<Animator>();
-        hitCover = hit.GetComponent<Animator>();
-    }
 
-    void Update()
-    {
-        spankArm.Spank();
-        legs.LegsUp();
-    }
-
-    public void KissWinAnimation()
-    {
-        if (kissTriggered == false)
-        {
-            heartAnim.SetTrigger("Success");
-            StartCoroutine(KissMove());
-            kissTriggered = true;
-        }
-    }
-
-    private IEnumerator KissMove()
-    {
-        richmondLipsRB.velocity = transform.right * -2;
-        yield return new WaitForSeconds(.5f);
-        richmondLipsRB.velocity = Vector3.zero;
-        yield return null;
-    }
-
-    public void KissLoseAnimation()
-    {
-        if (kissTriggered == false)
-        {
-            StartCoroutine(MissMove());
-            leftHeartAnim.SetTrigger("Break");
-            rightHeartAnim.SetTrigger("Break");
-            kissTriggered = true;
-        }
-    }
-
-    private IEnumerator MissMove()
-    {
-        missusRB.velocity = transform.right * -5;
-        yield return null;
-    }
-
-    public void HitWinAnimation()
-    {
-        if(hitTriggered == false){
-            spankArm.StartSpank();
-            spankMarkAnim.SetTrigger("Spank");
-            hitTriggered = true;
-        }
-    }
-
-    public void HitLoseAnimation()
-    {
-        if (hitTriggered == false)
-        {
-            legs.StartLegsUp();
-            hitTriggered = true;
-        }
-    }
-
-    public bool ReturnKissTriggered()
-    {
-        return kissTriggered;
-    }
-
-    public bool ReturnHitTriggered()
-    {
-        return hitTriggered;
-    }
-
-    public void CoverSwitch()
-    {
-        hitCover.SetTrigger("Cover");
-        kissCover.SetTrigger("Cover");
     }
 }
--- Assets/Scripts/Cheek to Cheek/Gameplay.cs
+++ Assets/Scripts/Cheek to Cheek/Gameplay.cs
@@ -10,8 +10,6 @@ public class Gameplay : MonoBehaviour
     [SerializeField] ScoreHandler scorehandler;
 
     [Header("Level Objects")]
-    [SerializeField] private RichmondLips richmondLips;
-    [SerializeField] private Spotlight spotlight;
     [SerializeField] private GameControls gamecontrols;
     [SerializeField] private AnimationController animationController;
 
@@ -25,7 +23,14 @@ public class Gameplay : MonoBehaviour
 
         gamecontrols = new GameControls();
         gamecontrols.Move.Select.performed += x => GameAction();
+
         StartCoroutine(GameSwitcher());
+    }
+
+    private IEnumerator GameSwitcher()
+    {
+        yield return new WaitForSeconds(measureMS * 3);
+        firstScenario = false;
     }
 
 
@@ -39,54 +44,15 @@ public class Gameplay : MonoBehaviour
         gamecontrols.Disable();
     }
 
-    private IEnumerator GameSwitcher()
-    {
-        yield return new WaitForSeconds(measureMS * 3);
-        firstScenario = false;
-        if (animationController.ReturnKissTriggered() == false)
-        {
-            richmondLips.stopAnimation();
-            animationController.KissLoseAnimation();
-        }
-        animationController.CoverSwitch();
-        yield return new WaitForSeconds(measureMS);
-        //countdowns[0].StartCountdown();
-        yield return new WaitForSeconds(measureMS * 3);
-        if (animationController.ReturnHitTriggered() == false)
-        {
-            animationController.HitLoseAnimation();
-        }
-    }
-
-    void Update()
-    {
-        spotlight.RotateSpotlight();
-    }
-
     private void GameAction()
     {
         if (firstScenario == true)
         {
-            richmondLips.stopAnimation();
-            if(richmondLips.transform.position.y > 2.5 && richmondLips.transform.position.y < 3.2)
-            {
-                animationController.KissWinAnimation();
-                scorehandler.IncrementScore();
-            } else
-            {
-                animationController.KissLoseAnimation();
-            }
-        } else
+            Debug.Log("First game");
+        }
+        else
         {
-            spotlight.StopRotating();
-            if(spotlight.transform.position.x > 2.9 && spotlight.transform.position.x < 3.7)
-            {
-                animationController.HitWinAnimation();
-                scorehandler.IncrementScore();
-            } else
-            {
-                animationController.HitLoseAnimation();
-            }
+            Debug.Log("Second game");
         }
     }
 }
