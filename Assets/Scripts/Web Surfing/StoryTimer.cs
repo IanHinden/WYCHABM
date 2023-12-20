7a6fb8ff2bb799051893fb2ad1ba01e9c897696f
Ian <Ian.Hinden@gmail.com>
2023-12-19 22:49:50 -0800

    Switch button color on pointer hover, sped up pointer animation and stopped in correct spot

--- Assets/Scripts/Web Surfing/StoryTimer.cs
+++ Assets/Scripts/Web Surfing/StoryTimer.cs
@@ -10,6 +10,9 @@ public class StoryTimer : MonoBehaviour
     [SerializeField] GameObject computerScreen;
     [SerializeField] GameObject pointer;
 
+    [SerializeField] GameObject greenLaptopScreen;
+    [SerializeField] GameObject purpleLaptopScreen;
+
     Animator pointerAnim;
 
     private SpriteRenderer pointerSR;
@@ -22,16 +25,21 @@ public class StoryTimer : MonoBehaviour
 
     public IEnumerator timedEvents()
     {
-
+        Debug.Log("Yeah");
         yield return new WaitForSeconds(timefunctions.ReturnCountMeasure(4));
         computerScreen.SetActive(true);
         pointerSR.enabled = true;
         pointerAnim.SetTrigger("Pointer");
+        yield return new WaitForSeconds(1.6f);
+        greenLaptopScreen.SetActive(false);
+        purpleLaptopScreen.SetActive(true);
     }
 
     public void Reset()
     {
         computerScreen.SetActive(false);
+        greenLaptopScreen.SetActive(true);
+        purpleLaptopScreen.SetActive(false);
         if (pointerSR != null)
         {
             pointerSR.enabled = false;
