ce402f512b53b9504d466833898ed84a7122b26a
Ian <Ian.Hinden@gmail.com>
2024-02-06 22:09:32 -0800

    Add second chorus art / Add punch effect connected to animation of fist

---
+++ Assets/Scripts/Mensch/MenschAudioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenschAudioController : MonoBehaviour
+{
+    [SerializeField] GameObject PunchSFX;
+
+    private AudioSource PunchSFXAS;
+
+    void Awake()
+    {
+        PunchSFXAS = PunchSFX.GetComponent<AudioSource>();
+    }
+
+    public void PlayPunch()
+    {
+        PunchSFXAS.Play();
+    }
+}
---
+++ Assets/Scripts/Mensch/RightHook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHook : MonoBehaviour
+{
+    [SerializeField] MenschAudioController menschAudioController;
+
+    private void PlayPunchSFX()
+    {
+        menschAudioController.PlayPunch();
+    }
+}
