fab20b8b2e82f5cb029ad8a34b6ae9ca18dfb195
Ian <Ian.Hinden@gmail.com>
2024-03-31 08:42:18 -0700

    Add pause screen ccard flip animation and SFX

--- Assets/Scripts/UI/MenuSFX.cs
+++ Assets/Scripts/UI/MenuSFX.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class MenuSFX : MonoBehaviour
 {
     [SerializeField] GameObject PauseSFX;
+    [SerializeField] GameObject CardFlipSFX;
 
     private AudioSource PauseSFXAS;
+    private AudioSource CardFlipSFXAS;
     void Awake()
     {
         PauseSFXAS = PauseSFX.GetComponent<AudioSource>();
+        CardFlipSFXAS = CardFlipSFX.GetComponent<AudioSource>();
     }
 
     public void PlayPause()
     {
         PauseSFXAS.Play();
+    }
+
+    public void PlayFlip()
+    {
+        CardFlipSFXAS.Play();
     }
 }
---
+++ Assets/Scripts/UI/PauseCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseCard : MonoBehaviour
+{
+    [SerializeField] MenuSFX menuSFX;
+
+    private void playFlip()
+    {
+        menuSFX.PlayFlip();
+    }
+}
