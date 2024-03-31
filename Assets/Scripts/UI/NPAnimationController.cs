5c876f7d8252c165565a6285e208868bc700845f
Ian <Ian.Hinden@gmail.com>
2024-03-30 18:56:49 -0700

    Swap arrow sprites to new ones in chorus / Add animation to number person 1

---
+++ Assets/Scripts/UI/NPAnimationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPAnimationController : MonoBehaviour
+{
+    [SerializeField] GameObject NP1;
+
+    private Animator NP1Anim;
+
+    void Awake()
+    {
+        NP1Anim = NP1.GetComponent<Animator>();
+    }
+}
