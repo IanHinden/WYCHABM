21a3695641dc298cde58a16f6b89aee5dd27f433
Ian <Ian.Hinden@gmail.com>
2024-04-04 20:03:25 -0700

    Add light animation to See Men

---
+++ Assets/Scripts/SeeMen/LightAnimator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightAnimator : MonoBehaviour
+{
+    [SerializeField] GameObject leftLight;
+    [SerializeField] GameObject middleLight;
+    [SerializeField] GameObject rightLight;
+
+    private SpriteRenderer leftLightSR;
+    private SpriteRenderer middleLightSR;
+    private SpriteRenderer rightLightSR;
+    void Start()
+    {
+        leftLightSR = leftLight.GetComponent<SpriteRenderer>();
+        middleLightSR = middleLight.GetComponent<SpriteRenderer>();
+        rightLightSR = rightLight.GetComponent<SpriteRenderer>();
+    }
+
+    private void LeftLightOn()
+    {
+        leftLightSR.enabled = true;
+    }
+
+    private void LeftLightOff()
+    {
+        leftLightSR.enabled = false;
+    }
+
+    private void MiddleLightOn()
+    {
+        middleLightSR.enabled = true;
+    }
+
+    private void MiddleLightOff()
+    {
+        middleLightSR.enabled = false;
+    }
+
+    private void RightLightOn()
+    {
+        rightLightSR.enabled = true;
+    }
+
+    private void RighttLightOff()
+    {
+        rightLightSR.enabled = false;
+    }
+}
