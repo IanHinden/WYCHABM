1b6f56f47b9b85026f197e3442c3df8fd62ff336
Ian <Ian.Hinden@gmail.com>
2024-04-04 21:04:13 -0700

    Add animated stamp and SFX to Fired

---
+++ Assets/Scripts/Fired/YoureFiredStamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoureFiredStamp : MonoBehaviour
+{
+    [SerializeField] GameObject StampSound;
+
+    private AudioSource StampSoundAS;
+    void Awake()
+    {
+        StampSoundAS = StampSound.GetComponent<AudioSource>();
+    }
+
+    private void PlayStamp()
+    {
+        StampSoundAS.Play();
+    }
+}
