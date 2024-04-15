3d27dcd0d59980a35e4299b466c4c6af1e012002
Ian <Ian.Hinden@gmail.com>
2024-04-14 18:44:45 -0700

    Add initial timing and animation logic for final score grade stamps

--- Assets/Scripts/ScoreScreen/FinalScore.cs
+++ Assets/Scripts/ScoreScreen/FinalScore.cs
@@ -10,6 +10,11 @@ public class FinalScore : MonoBehaviour
 
     [SerializeField] GameObject scoreScreen;
 
+    [SerializeField] ScoreStamp scoreStampPart1;
+    [SerializeField] ScoreStamp scoreStampPart2;
+    [SerializeField] ScoreStamp scoreStampPart3;
+    [SerializeField] ScoreStamp scoreStampFinal;
+
     [SerializeField] AudioSource gameOverTheme;
 
     private string standardScore;
@@ -25,10 +30,22 @@ public class FinalScore : MonoBehaviour
         standardScore = scoreHandler.ReturnScore().ToString();
         bonusScore = scoreHandler.ReturnBonusScore().ToString();
 
-        uiHandler.SetScoreScreenStandardScore(standardScore + "/16");
-        uiHandler.SetScoreScreenBonusScore(bonusScore + "/10");
+        uiHandler.SetScoreScreenStandardScore("");
+        uiHandler.SetScoreScreenBonusScore("");
 
         yield return new WaitForSeconds(.3f);
         gameOverTheme.Play();
+        yield return new WaitForSeconds(1.9f);
+        scoreStampPart1.SetGrade("A");
+        scoreStampPart1.AnimateStamp();
+        yield return new WaitForSeconds(.635f);
+        scoreStampPart2.SetGrade("D");
+        scoreStampPart2.AnimateStamp();
+        yield return new WaitForSeconds(.635f);
+        scoreStampPart3.SetGrade("D");
+        scoreStampPart3.AnimateStamp();
+        yield return new WaitForSeconds(1.27f);
+        scoreStampFinal.SetGrade("B");
+        scoreStampFinal.AnimateStamp();
     }
 }
---
+++ Assets/Scripts/ScoreScreen/ScoreStamp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreStamp : MonoBehaviour
+{
+    [SerializeField] Image RotateCat;
+    [SerializeField] Image SGrade;
+    [SerializeField] Image AGrade;
+    [SerializeField] Image BGrade;
+    [SerializeField] Image CGrade;
+    [SerializeField] Image DGrade;
+    [SerializeField] Image FGrade;
+
+    Animator stampAnim;
+
+    private void Awake()
+    {
+        stampAnim = this.GetComponent<Animator>();
+    }
+
+    public void SetGrade(string grade)
+    {
+        HideAllGrade();
+
+        if (grade == "S")
+        {
+            SGrade.enabled = true;
+        } else if (grade == "A")
+        {
+            AGrade.enabled = true;
+        } else if (grade == "B")
+        {
+            BGrade.enabled = true;
+        } else if (grade == "C")
+        {
+            CGrade.enabled = true;
+        } else if (grade == "D")
+        {
+            DGrade.enabled = true;
+        } else if (grade == "F")
+        {
+            FGrade.enabled = true;
+        }
+
+        Debug.Log(grade);
+    }
+
+    public void HideAllGrade()
+    {
+        SGrade.enabled = false;
+        AGrade.enabled = false;
+        BGrade.enabled = false;
+        CGrade.enabled = false;
+        DGrade.enabled = false;
+        FGrade.enabled = false;
+    }
+
+    public void AnimateStamp()
+    {
+        stampAnim.SetBool("ScoreStamp", true);
+        RotateCat.enabled = true;
+    }
+}
