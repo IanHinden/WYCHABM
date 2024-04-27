aae4d48f23780fe3aa888f27242c9033dc998847
Ian <Ian.Hinden@gmail.com>
2024-04-26 22:02:02 -0700

    Add animation for sample instructions

---
+++ Assets/Scripts/UI/InstructionExample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InstructionExample : MonoBehaviour
+{
+    private TextMeshProUGUI instructionText;
+    private string[] instructionExamples = new string[4];
+    int curr = 0;
+    private void Awake()
+    {
+        instructionText = this.gameObject.GetComponent<TextMeshProUGUI>();
+        instructionExamples[0] = "COLLECT";
+        instructionExamples[1] = "SELECT";
+        instructionExamples[2] = "DRIVE";
+        instructionExamples[3] = "AIM";
+    }
+
+    private void InstructionSetText()
+    {
+        instructionText.text = instructionExamples[curr];
+        curr++;
+        if(curr > instructionExamples.Length - 1)
+        {
+            curr = 0;
+        }
+    }
+
+    private void HideText()
+    {
+        instructionText.text = "";
+    }
+}
