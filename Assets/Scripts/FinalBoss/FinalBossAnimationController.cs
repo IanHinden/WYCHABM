93709a0f1fab192522e3cb5961e62ddb077fafd7
Ian <Ian.Hinden@gmail.com>
2024-05-26 22:06:44 -0700

    Add function to fade in Final Boss and add Ava Front View sprites

--- Assets/Scripts/FinalBoss/FinalBossAnimationController.cs
+++ Assets/Scripts/FinalBoss/FinalBossAnimationController.cs
@@ -7,7 +7,18 @@ public class FinalBossAnimationController : MonoBehaviour
     [SerializeField] TimeFunctions timeFunctions;
 
     [SerializeField] SpriteRenderer AvaTarts;
+
+    [Header("Ava Sees Richman Objects")]
     [SerializeField] GameObject AvaSeesRichman;
+    [SerializeField] SpriteRenderer AvaSeesRichmanHappy;
+    [SerializeField] SpriteRenderer AvaSeesRichmanSad;
+    [SerializeField] SpriteRenderer AvaSeesRichmanEyes;
+
+    [Header("Ava Front View Objects")]
+    [SerializeField] GameObject AvaFrontView;
+
+    private float AvaSeesRichmanTransitionDuration = .5f;
+
     private void Awake()
     {
         StartCoroutine(SceneTiming());
@@ -18,5 +29,23 @@ public class FinalBossAnimationController : MonoBehaviour
         yield return new WaitForSeconds(timeFunctions.ReturnCountMeasure(4));
         AvaTarts.enabled = false;
         AvaSeesRichman.SetActive(true);
+        yield return new WaitForSeconds(timeFunctions.ReturnCountMeasure(1));
+        StartCoroutine(AvaSeesRichmanFade(AvaSeesRichmanHappy, AvaSeesRichmanTransitionDuration, true));
+        StartCoroutine(AvaSeesRichmanFade(AvaSeesRichmanSad, AvaSeesRichmanTransitionDuration, false));
+        StartCoroutine(AvaSeesRichmanFade(AvaSeesRichmanEyes, AvaSeesRichmanTransitionDuration, false));
+    }
+
+    private IEnumerator AvaSeesRichmanFade(SpriteRenderer spriteRend, float fadeDuration, bool inOrOut)
+    {
+        float elapsedTime = 0;
+        Color originalColor = spriteRend.color;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = inOrOut ? Mathf.Lerp(1, 0, elapsedTime / fadeDuration) : Mathf.Lerp(0, 1, elapsedTime / fadeDuration);
+            spriteRend.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
     }
 }
