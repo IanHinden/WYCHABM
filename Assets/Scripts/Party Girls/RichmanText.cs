747db0008ed37ba11158d8f717c0d480ef4c1a99
Ian <Ian.Hinden@gmail.com>
2024-06-20 21:22:35 -0700

    Add text message sound

---
+++ Assets/Scripts/Party Girls/RichmanText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RichmanText : MonoBehaviour
+{
+    [SerializeField] AudioSource bubbleSFX;
+
+    private void playBubbleSFX()
+    {
+        bubbleSFX.Play();
+    }
+}
