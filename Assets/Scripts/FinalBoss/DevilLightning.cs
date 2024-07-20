f80545ac27d72b9edb6e2186360a85d157d67024
Ian <Ian.Hinden@gmail.com>
2024-07-19 22:18:30 -0700

    Add lightning flashes and slower timing to devil richman

---
+++ Assets/Scripts/FinalBoss/DevilLightning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilLightning : MonoBehaviour
+{
+    private SpriteRenderer lightningSR;
+
+    private void Awake()
+    {
+        lightningSR = this.GetComponent<SpriteRenderer>();
+    }
+    private void LightningStrike()
+    {
+        StartCoroutine(lightningAnimation());
+    }
+
+    private IEnumerator lightningAnimation()
+    {
+        int flashes = 3;
+        while (flashes > 0)
+        {
+            lightningSR.enabled = true;
+            yield return new WaitForSeconds(.05f);
+            lightningSR.enabled = false;
+            yield return new WaitForSeconds(.05f);
+            flashes--;
+        }
+    }
+}
